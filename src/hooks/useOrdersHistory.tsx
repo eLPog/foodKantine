8b87768cc3874969d61f4aa40de91b22545b1758
LukasP <lukas.straubing@gmail.com>
2022-11-30 18:41:26 +0100

    custom hool typed = useOrdersHistory

--- src/hooks/useOrdersHistory.tsx
+++ src/hooks/useOrdersHistory.tsx
@@ -2,12 +2,18 @@ import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { firebaseURL } from '../assets/db/firebaseurl';
 import { isAuthenticatedContext } from '../context/isAuthenticatedContext';
-import { orderFromFetchInterface } from '../interfaces/orderObjectInterface';
+import { orderFromFetchInterface, orderObjectInterface } from '../interfaces/orderObjectInterface';
 
+interface stateObject {
+  allUserOrders:[] | orderFromFetchInterface[],
+  lastOrder:null | orderObjectInterface,
+  isLoading:boolean,
+  totalValue:number
+}
 export function useOrdersHistory() {
   const { userState } = useContext(isAuthenticatedContext);
   const navigate = useNavigate();
-  const [ordersHistory, setOrdersHistory] = useState({
+  const [ordersHistory, setOrdersHistory] = useState<stateObject>({
     allUserOrders: [], lastOrder: null, isLoading: true, totalValue: 0,
   });
   useEffect(() => {
@@ -17,12 +23,12 @@ export function useOrdersHistory() {
     (async function getData() {
       try {
         const data = await fetch(`${firebaseURL}orders.json`);
-        let res:orderFromFetchInterface[] = await data.json();
-        res = Object.values(res);
-        res = res.filter((el) => el.userID === userState.localId);
+        const arrOfAllOrders = await data.json();
+        let res:orderFromFetchInterface[] = Object.values(arrOfAllOrders);
+        res = res.filter((el:orderFromFetchInterface) => el.userID === userState.localId);
         res = res.reverse();
         let totalValue = 0;
-        res.forEach((el) => el.meals.forEach((el) => totalValue += el.price));
+        res.forEach((el:orderFromFetchInterface) => el.meals.forEach((el) => totalValue += el.price));
         setOrdersHistory({
           allUserOrders: res, lastOrder: res[0], isLoading: false, totalValue,
         });
